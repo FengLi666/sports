cbb5097b0fb6f5603690087dd19cbfb26d31f024
FengLi666 <fengli97@hotmail.com>
2018-10-16 18:27:33 +0800

    Initial commit

---
+++ login.py
@@ -0,0 +1,62 @@
+import json
+import traceback
+import uuid
+
+import requests
+
+from free_run import free_run
+from no_free_run import no_free_run
+from original_json import headers, host
+from sports import get_md5_code
+
+
+def login(mobile, psd):
+    headers['uuid'] = uuid.uuid4().hex.upper()
+    s = requests.Session()
+
+    s.headers = headers
+
+    # get cookie
+    data = r'{}'
+    s.get(host + '/api/configuration/apiConfig', params={'sign': get_md5_code(data), 'data': data})
+
+    login_data = json.dumps(
+        {"info": headers['uuid'], "mobile": str(mobile), "password": str(psd), "type": "HUAWEIMLA-AL10"})
+
+    login_res = s.get(host + '/api/reg/login', params={'sign': get_md5_code(login_data), 'data': login_data})
+    print(login_res.text)
+    login_rd = login_res.json()
+
+    userid = login_rd['data']['userid']
+    utoken = login_rd['data']['utoken']
+
+    s.headers.update({'utoken': utoken})
+    return (userid, s)
+
+if __name__=='__main__':
+
+    try:
+        userid, s = login('18616805603', '123qwe')
+    except Exception as e:
+        traceback.print_exc()
+        print('login failed')
+
+    print('loging successfully')
+    type = input('not_free :0  free: 1\n')
+    while type not in ('1','0'):
+        print('1 or 0 please !!!')
+        type = input('not_free :0  free: 1\n')
+    type = int(type)
+    while 1:
+        x = input('distance or quit\n')
+        if x=='quit':
+            break;
+        try:
+            if type:
+                free_run(userid, s, dis=float(x))
+            else:
+                no_free_run(userid, s, dis=float(x))
+        except Exception as e:
+            traceback.print_exc()
+            print('something wrong, try again')
+
---
+++ no_free_run.py
@@ -0,0 +1,41 @@
+import json
+from datetime import datetime, timedelta
+
+from original_json import *
+from sports import *
+
+
+def no_free_run(userid: str, ses, dis: float = 2, start_point=gps_point(30.8669741312, 121.9183560969)):
+    data = json.dumps({"initLocation": "121.85284044053819,30.911461588541666", "type": "1", "userid": "156766"})
+
+    res = ses.get(host + '/api/run/runPage', params={'sign': get_md5_code(data), 'data': data.encode('ascii')})
+
+    resj = res.json()['data']
+
+    x = {"buPin": "0.0", "duration": "1000",
+         "endTime": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+         "startTime": (datetime.now() - timedelta(seconds=1000)).strftime("%Y-%m-%d %H:%M:%S"),
+         "frombp": "0", "goal": "2.00",
+         "totalNum": "0",
+         "trend": [], "type": "1",
+         "userid": userid, 'real': str(dis * 1000), 'runPageId': resj['runPageId'], 'speed': "3'40''",
+         'bNode': resj['ibeacon'], 'tNode': resj['gpsinfo']}
+
+    gpl = gps_point_list()
+    gpl.run(start_point=start_point, stripe=0.0001)
+    x['track'] = gpl.get_track()
+    for node in x['bNode']:
+        pos = node['position']
+        pos['latitude'] = float(pos['latitude'])
+        pos['longitude'] = float(pos['longitude'])
+        pos['speed'] = 0.0
+        node['position'] = pos
+    for pos in x['tNode']:
+        pos['latitude'] = float(pos['latitude'])
+        pos['longitude'] = float(pos['longitude'])
+        pos['speed'] = 0.0
+
+    xs = json.dumps(x)
+
+    r = ses.post(host + '/api/run/saveRunV2', data={'sign': get_md5_code(xs), 'data': xs.encode('ascii')})
+    print(r.content.decode('utf-8'))
